2e60e3ac1fac96211316dc5d03f06c48aaef84bb
boscannon <p12576999gmail.com>
2019-05-29 12:40:43 +0800

    9

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use Cannon\Comparison\Comparison;
 $x=new Comparison();
 
 $arr=[
-	'pp'=>["pp"=>3453,234,32525,235],
+	'pp'=>["pp"=>3453,234,'ewge',235],
 	["pewfgewgp"=>123,'2324']
 ];
 $x->show($arr,['length'=>30,'index'=>true]);
